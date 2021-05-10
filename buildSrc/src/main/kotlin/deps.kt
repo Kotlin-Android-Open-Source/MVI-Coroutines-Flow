5c0e460fc481dcaf5a63e407c9f12bdd0245f9ee
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-05-10 17:40:07 +0700

    coroutine 1.5.0-RC

--- buildSrc/src/main/kotlin/deps.kt
+++ buildSrc/src/main/kotlin/deps.kt
@@ -46,7 +46,7 @@ object deps {
   }
 
   object coroutines {
-    private const val version = "1.4.3"
+    private const val version = "1.5.0-RC"
 
     const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
     const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
