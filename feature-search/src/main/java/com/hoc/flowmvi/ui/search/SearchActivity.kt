1a8e9ee342923a05effc7b8f5209f1907add240d
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-05-11 15:32:36 +0700

    wip

--- feature-search/src/main/java/com/hoc/flowmvi/ui/search/SearchActivity.kt
+++ feature-search/src/main/java/com/hoc/flowmvi/ui/search/SearchActivity.kt
@@ -5,8 +5,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hoc.flowmvi.core.navigator.IntentProviders
+import com.hoc.flowmvi.ui.search.databinding.ActivitySearchBinding
 
-class SearchActivity : AppCompatActivity(R.layout.activity_search) {
+class SearchActivity : AppCompatActivity() {
+  private val addBinding by lazy(LazyThreadSafetyMode.NONE) { ActivitySearchBinding.inflate(layoutInflater) }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(addBinding.root)
+    supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+  }
 
   internal class IntentProvider : IntentProviders.Search {
     override fun makeIntent(context: Context): Intent =
