8f3982a95873bd868e7cb5a039b943afc0c6cce9
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-05-11 15:21:38 +0700

    init

---
+++ feature-search/src/main/java/com/hoc/flowmvi/ui/search/SearchActivity.kt
@@ -0,0 +1,8 @@
+package com.hoc.flowmvi.ui.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchActivity : AppCompatActivity(R.layout.activity_search) {
+
+}
