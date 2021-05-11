5758ef53de7e5462646939927fbda5f54a722902
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-05-11 15:34:06 +0700

    wip

--- feature-search/src/main/java/com/hoc/flowmvi/ui/search/SearchActivity.kt
+++ feature-search/src/main/java/com/hoc/flowmvi/ui/search/SearchActivity.kt
@@ -4,16 +4,28 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.hoc.flowmvi.core.navigator.IntentProviders
 import com.hoc.flowmvi.ui.search.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
-  private val addBinding by lazy(LazyThreadSafetyMode.NONE) { ActivitySearchBinding.inflate(layoutInflater) }
+  private val addBinding by lazy(LazyThreadSafetyMode.NONE) {
+    ActivitySearchBinding.inflate(
+      layoutInflater
+    )
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(addBinding.root)
     supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    return when (item.itemId) {
+      android.R.id.home -> finish().let { true }
+      else -> super.onOptionsItemSelected(item)
+    }
   }
 
   internal class IntentProvider : IntentProviders.Search {
