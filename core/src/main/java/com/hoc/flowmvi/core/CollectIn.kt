fa29c5fdfe192e9d5d1ac4483578b5a644bb08a2
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-11-02 18:01:38 +0700

    use repeatOnLifecycle

--- core/src/main/java/com/hoc/flowmvi/core/CollectIn.kt
+++ core/src/main/java/com/hoc/flowmvi/core/CollectIn.kt
@@ -4,32 +4,32 @@ import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.addRepeatingJob
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
+import kotlinx.coroutines.launch
 
 inline fun <T> Flow<T>.collectIn(
   owner: LifecycleOwner,
   minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
-  coroutineContext: CoroutineContext = EmptyCoroutineContext,
   crossinline action: suspend (value: T) -> Unit,
-): Job = owner.addRepeatingJob(state = minActiveState, coroutineContext = coroutineContext) {
-  Log.d("collectIn", "Start collecting...")
-  collect { action(it) }
+): Job = owner.lifecycleScope.launch {
+  owner.lifecycle.repeatOnLifecycle(state = minActiveState) {
+    Log.d("collectIn", "Start collecting $owner $minActiveState...")
+    collect { action(it) }
+  }
 }
 
 @Suppress("unused")
 inline fun <T> Flow<T>.collectIn(
   fragment: Fragment,
   minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
-  coroutineContext: CoroutineContext = EmptyCoroutineContext,
   crossinline action: suspend (value: T) -> Unit,
-): Job = collectIn(
-  fragment.viewLifecycleOwner,
-  minActiveState = minActiveState,
-  coroutineContext = coroutineContext,
-  action = action,
-)
+): Job = fragment.lifecycleScope.launch {
+  fragment.viewLifecycleOwner.repeatOnLifecycle(state = minActiveState) {
+    Log.d("collectIn", "Start collecting $fragment $minActiveState...")
+    collect { action(it) }
+  }
+}
