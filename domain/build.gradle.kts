4d2389c2e5d9e4a27a66a87092775292883f0e4f
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-11-06 17:01:03 +0700

    targetCompatibility = JavaVersion.VERSION_1_8

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
   kotlin
 }
 
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 dependencies {
   implementation(deps.coroutines.core)
   implementation(deps.koin.core)
