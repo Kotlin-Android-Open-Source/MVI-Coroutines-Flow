d22423c00b1be802329016913083a78a7db4d6b6
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-11-07 18:17:36 +0700

    fix(add): `isLoading` of restored state should be `false`

--- feature-add/src/main/java/com/hoc/flowmvi/ui/add/AddVM.kt
+++ feature-add/src/main/java/com/hoc/flowmvi/ui/add/AddVM.kt
@@ -39,7 +39,8 @@ class AddVM(
   override val viewState: StateFlow<ViewState>
 
   init {
-    val initialVS = savedStateHandle.get<ViewState?>(VIEW_STATE) ?: ViewState.initial()
+    val initialVS = savedStateHandle.get<ViewState?>(VIEW_STATE).copy(isLoading = false)
+      ?: ViewState.initial()
     Timber.tag(logTag).d("[ADD_VM] initialVS: $initialVS")
 
     viewState = intentFlow
