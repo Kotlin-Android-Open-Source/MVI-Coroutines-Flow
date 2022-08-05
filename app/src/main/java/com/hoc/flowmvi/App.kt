bcc223b5a902c66168b421ed02fbc03d0bd4dddf
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2022-08-05 20:40:45 +0700

    deps: flowExt and ViewBindingDelegate

--- app/src/main/java/com/hoc/flowmvi/App.kt
+++ app/src/main/java/com/hoc/flowmvi/App.kt
@@ -15,7 +15,6 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
-import timber.log.Timber
 
 @FlowPreview
 @ExperimentalCoroutinesApi
@@ -41,12 +40,6 @@ class App : Application() {
     super.onCreate()
 
     DynamicColors.applyToActivitiesIfAvailable(this)
-
-    if (BuildConfig.DEBUG) {
-      Timber.plant(Timber.DebugTree())
-    } else {
-      // TODO(Timber): plant release tree
-    }
 
     startKoin {
       androidContext(this@App)
