e73d5fed7810b0026414ecfc82b7744f412173d4
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2023-02-19 22:27:28 +0700

    simplify

--- data/src/main/java/com/hoc/flowmvi/data/UserRepositoryImpl.kt
+++ data/src/main/java/com/hoc/flowmvi/data/UserRepositoryImpl.kt
@@ -16,6 +16,7 @@ import com.hoc.flowmvi.domain.model.User
 import com.hoc.flowmvi.domain.model.UserError
 import com.hoc.flowmvi.domain.model.UserValidationError
 import com.hoc.flowmvi.domain.repository.UserRepository
+import com.hoc081098.flowext.flowFromSuspend
 import com.hoc081098.flowext.retryWithExponentialBackoff
 import java.io.IOException
 import kotlin.time.Duration.Companion.milliseconds
@@ -24,7 +25,6 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flatMapConcat
@@ -51,29 +51,21 @@ internal class UserRepositoryImpl(
     class Added(val user: User) : Change()
   }
 
-  private val responseToDomainThrows: (UserResponse) -> User = { response ->
-    responseToDomain(response).let { validated ->
-      validated.getOrElse {
-        val t = UserError.ValidationFailed(it)
-        logError(t, "Map $response to user")
-        throw t
-      }
-    }
-  }
-
   private val changesFlow = MutableSharedFlow<Change>(extraBufferCapacity = 64)
-
   private suspend inline fun sendChange(change: Change) = changesFlow.emit(change)
 
-  @Suppress("NOTHING_TO_INLINE")
-  private inline fun logError(t: Throwable, message: String) = Timber.tag(TAG).e(t, message)
-
-  private fun getUsersFromRemote(): Flow<List<User>> = suspend {
+  private fun getUsersFromRemote(): Flow<List<User>> = flowFromSuspend {
     Timber.d("[USER_REPO] getUsersFromRemote ...")
+
     userApiService
       .getUsers()
-      .map(responseToDomainThrows)
-  }.asFlow()
+      .map { response ->
+        responseToDomain(response)
+          .mapLeft(UserError::ValidationFailed)
+          .onLeft { logError(it, "Map $response to user") }
+          .getOrElse { throw it }
+      }
+  }
     .retryWithExponentialBackoff(
       maxAttempt = 2,
       initialDelay = 500.milliseconds,
@@ -121,7 +113,7 @@ internal class UserRepositoryImpl(
     }
   }
 
-  override suspend fun add(user: User) = either<UserError, Unit> {
+  override suspend fun add(user: User) = either {
     withContext(dispatchers.io) {
       val response = catchEither { userApiService.add(domainToBody(user)) }
         .onLeft { logError(it, "add user=$user") }
@@ -137,13 +129,28 @@ internal class UserRepositoryImpl(
     }
   }
 
-  override suspend fun search(query: String) = withContext(dispatchers.io) {
-    catchEither { userApiService.search(query).map(responseToDomainThrows) }
-      .onLeft { logError(it, "search query=$query") }
-      .mapLeft(errorMapper)
+  override suspend fun search(query: String) = either {
+    withContext(dispatchers.io) {
+      val userResponses = catchEither { userApiService.search(query) }
+        .onLeft { logError(it, "search query=$query") }
+        .mapLeft(errorMapper)
+        .bind()
+
+      val users = userResponses.map { userResponse ->
+        responseToDomain(userResponse)
+          .mapLeft(UserError::ValidationFailed)
+          .onLeft { logError(it, "search query=$query") }
+          .bind()
+      }
+
+      users
+    }
   }
 
   private companion object {
+    @Suppress("NOTHING_TO_INLINE")
+    private inline fun logError(t: Throwable, message: String) = Timber.tag(TAG).e(t, message)
+
     private val TAG = UserRepositoryImpl::class.java.simpleName
   }
 }
